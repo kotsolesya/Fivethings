5c7f4fe256ac4250b59caf6796877f03b497f99f
olesya <kots.olesya@gmail.com>
2016-04-14 23:00:36 +0300

    all functional running

--- app/src/main/java/com/example/olesya/fivethings/LastActivity.java
+++ app/src/main/java/com/example/olesya/fivethings/LastActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,6 +15,7 @@ import android.view.ViewConfiguration;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -22,23 +24,122 @@ import java.lang.reflect.Field;
 public class LastActivity extends AppCompatActivity {
     String statusFromRadioGroup;
     String nameUser;
+    String textCheckboxWasHelpful;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last);
-
-
+      // встановлення ActionBar кнопки home і відгук
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_action_home);
+      //  обробка RadioGroup
+       RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup_who_are_you);
+        /*RadioButton rbu =(RadioButton)findViewById(R.id.radio_btn_junior);
+        rbu.getId();
+        rbu.setChecked(true);
+        statusFromRadioGroup = rbu.getText().toString();*/
+         checkRadioGroup(radioGroup);
+
+
+      // кнопка Підсумок
         setSubmitButton();
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup_who_are_you);
-        statusFromRadioGroup = checkRadioGroup(radioGroup);
+
         Log.i("TAG", "Create_Last");
 
 
     }
+
+
+    /*
+    обробка RadioGroup
+     */
+    private void checkRadioGroup(RadioGroup radioGroup) {
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton =  (RadioButton) group.findViewById(checkedId);
+
+                switch (checkedId) {
+                    case -1:
+                        statusFromRadioGroup = "no status";
+                        break;
+                    case R.id.radio_btn_junior:
+                        statusFromRadioGroup = radioButton.getText().toString();
+                        break;
+                    case R.id.radio_btn_middle:
+                        statusFromRadioGroup = radioButton.getText().toString();
+                        break;
+                    case R.id.radio_btn_senior:
+                        statusFromRadioGroup = radioButton.getText().toString();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+
+    }
+
+
+    /*
+     * setSubmitButton for button Submit, create resume
+       @param builder для діалогового вікна, що підсумовує відповіль для користувача після натиснення кнопки Submit
+     */
+    private void setSubmitButton() {
+        Button btnFirst = (Button) findViewById(R.id.btn_submit);
+        if(statusFromRadioGroup==null) statusFromRadioGroup="Junior Developer";
+
+        // обробчик клікання кнопки
+        if (btnFirst != null) {
+            btnFirst.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   // Log.i("TAG", statusFromRadioGroup);
+                    EditText editTextName = (EditText) findViewById(R.id.editText_name);
+                    nameUser = editTextName.getText().toString();
+                    if(TextUtils.isEmpty(nameUser)) {
+                        editTextName.setError("please enter your name");
+                        return;
+                    }
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LastActivity.this);
+                    builder.setTitle("Thank you ," + nameUser + ", for your time!")
+                            .setMessage("You are " + statusFromRadioGroup + "."+ checkCheckboxWasHelpful() )
+                            .setCancelable(false)
+                            .setNegativeButton("ОК",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+
+
+                }
+            });
+        }
+
+    }
+
+private String checkCheckboxWasHelpful(){
+    //  обробка CheckBox
+    String text =new String();
+    CheckBox checkboxWasHelpful = (CheckBox)findViewById(R.id.checkBox_was_helpful);
+    if (checkboxWasHelpful.isChecked() )
+    {
+        text = " I am happy, that this application is useful";
+    }
+    else {
+        text = " Don't judge me too harshly:)";
+    }
+    return text;
+}
 
     @Override
     protected void onStop() {
@@ -61,72 +162,5 @@ public class LastActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    /*
-    обробка RadioGroup
-     */
-    private String checkRadioGroup(RadioGroup radioGroup) {
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                switch (checkedId) {
-                    case -1:
-                        Toast.makeText(getApplicationContext(), "No choice", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.radio_btn_junior:
-                        Toast.makeText(getApplicationContext(), "junior", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.radio_btn_middle:
-                        Toast.makeText(getApplicationContext(), "middle", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.radio_btn_senior:
-                        Toast.makeText(getApplicationContext(), "senior", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-        });
-        return "junior";
-    }
-
-    /*
-     * setSubmitButton for button Submit, create resume
-       @param builder для діалогового вікна, що підсумовує відповіль для користувача після натиснення кнопки Submit
-     */
-    private void setSubmitButton() {
-        Button btnFirst = (Button) findViewById(R.id.btn_submit);
-
-        // обробчик клікання кнопки
-        if (btnFirst != null) {
-            btnFirst.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.i("TAG", statusFromRadioGroup);
-                    EditText editTextName = (EditText) findViewById(R.id.editText_name);
-                    nameUser = editTextName.getText().toString();
-                    AlertDialog.Builder builder = new AlertDialog.Builder(LastActivity.this);
-                    builder.setTitle("Thank you," + nameUser + "for your feedback!")
-                            .setMessage("Покормите кота!" + statusFromRadioGroup)
-                            .setCancelable(false)
-                            .setNegativeButton("ОК",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            dialog.cancel();
-                                        }
-                                    });
-                    AlertDialog alert = builder.create();
-                    alert.show();
-
-
-                }
-            });
-        }
-
     }
 }
